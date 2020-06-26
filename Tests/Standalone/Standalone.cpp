48233bfa50980994da57b8ae9ccc4ea9bb91c63d
Marco Francescangeli <mafranc@microsoft.com>
2020-06-26 15:41:47 +0100

    test with Qt

--- Tests/Standalone/Standalone.cpp
+++ Tests/Standalone/Standalone.cpp
@@ -1,9 +1,15 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
+#include <QDir>
+#include <QStandardPaths>
+
 // dummy test. To test the framework
 TEST(sample_test_case_standalone, sample_test_to_fail)
 {
+    QDir appDataRootDir = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+    std::cout << appDataRootDir.path().toStdString() << std::endl;
+	
     EXPECT_EQ(2, 3);
 }
 
