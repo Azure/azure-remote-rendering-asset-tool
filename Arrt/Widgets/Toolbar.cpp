240b9c82048b35a87f80a14e0758096854ca6cc9
Marco Francescangeli <mafranc@microsoft.com>
2020-11-25 12:51:45 +0000

    Toolbar using flow layout

--- Arrt/Widgets/Toolbar.cpp
+++ Arrt/Widgets/Toolbar.cpp
@@ -1,20 +1,20 @@
 #include <QHBoxLayout>
 #include <View/ArrtStyle.h>
+#include <Widgets/FlowLayout.h>
 #include <Widgets/Toolbar.h>
 #include <Widgets/ToolbarButton.h>
 
 Toolbar::Toolbar(QWidget* parent)
     : FormControl(parent)
 {
-    m_layout = new QHBoxLayout();
+    setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Minimum);
+    m_layout = new FlowLayout(nullptr, 0, 5, 5);
     m_layout->setContentsMargins(0, 0, 0, 0);
-    m_layout->setSpacing(10);
-    m_layout->addStretch();
     setLayout(m_layout);
     setHeader("");
 }
 
 void Toolbar::addButton(ToolbarButton* button)
 {
-    m_layout->insertWidget(m_layout->count() - 1, button);
+    m_layout->addWidget(button);
 }
