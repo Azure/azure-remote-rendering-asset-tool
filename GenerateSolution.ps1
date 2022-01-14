9392357c97f081d045c68b5946b9b88289f6aa28
Jan Krassnigg <jakra-ms@outlook.com>
2022-01-14 17:29:23 +0100

    Use existing vcpkg for buddy builds

--- GenerateSolution.ps1
+++ GenerateSolution.ps1
@@ -1,7 +1,8 @@
 param 
 (
 	[Parameter(Mandatory = $False)] [string] $Destination = "$PSScriptRoot\Workspace",
-	[Parameter(Mandatory = $False)] [ValidateSet('vs2019', 'vs2022')] $Solution = "vs2019"
+	[Parameter(Mandatory = $False)] [ValidateSet('vs2019', 'vs2022')] $Solution = "vs2019",
+	[Parameter(Mandatory = $False)] [string] $Vcpkg = ""
 )
 
 Write-Host ""
@@ -11,64 +12,70 @@ Write-Host "Destination Path: $Destination"
 Write-Host "Solution for: $Solution"
 & cmake.exe --version
 
-if ($null -eq (Get-Command "nuget.exe" -ErrorAction SilentlyContinue)) 
-{ 
+if ($null -eq (Get-Command "nuget.exe" -ErrorAction SilentlyContinue)) { 
 	throw "Unable to find nuget.exe in your PATH environment variable."
 }
 
-if ($null -eq (Get-Command "cmake.exe" -ErrorAction SilentlyContinue)) 
-{ 
+if ($null -eq (Get-Command "cmake.exe" -ErrorAction SilentlyContinue)) { 
 	throw "Unable to find cmake.exe in your PATH environment variable."
 }
 
-if ($null -eq $env:Qt6_DIR)
-{ 
+if ($null -eq $env:Qt6_DIR) { 
 	Write-Host "Environment variable 'Qt6_DIR' is not set."
 	#throw "Environment variable 'Qt6_DIR' is not set."
 }
-else 
-{
+else {
 	Write-Host "Environment variable Qt6_DIR is set to: '$env:Qt6_DIR'."
 }
 
-if (-not ($null -eq $env:Qt6_DIR) -and (-not (Test-Path $env:Qt6_DIR)))
-{ 
+if (-not ($null -eq $env:Qt6_DIR) -and (-not (Test-Path $env:Qt6_DIR))) { 
 	#throw "Environment variable Qt6_DIR points to non-existing directory: '$env:Qt6_DIR'."
 }
 
 $CacheFile = $Destination + "\CMakeCache.txt"
-$VcpkgPath = $Destination + "\vcpkg"
 
 if (Test-Path -Path $CacheFile -PathType leaf) {
 	Remove-Item $CacheFile
 }
 
-Write-Host ""
-Write-Host "=== Running vcpkg ==="
-Write-Host ""
+if ("" -eq $Vcpkg) {
+	$VcpkgPath = $Destination + "\vcpkg"
 
-if ((Test-Path -Path $VcpkgPath) -eq $false) {
-	git clone https://github.com/microsoft/vcpkg "$Destination\vcpkg"
+	Write-Host ""
+	Write-Host "=== Setting up vcpkg ==="
+	Write-Host ""
+
+	if ((Test-Path -Path $VcpkgPath) -eq $false) {
+		git clone https://github.com/microsoft/vcpkg "$Destination\vcpkg"
 	
-	# could check out a fixed commit, but we'll just try latest for now
+		# could check out a fixed commit, but we'll just try latest for now
+	}
+
+	# bootstrap vcpkg
+	&$VcpkgPath\bootstrap-vcpkg.bat
+
+	# get Azure Storage SDK through vcgpk
+	&$VcpkgPath\vcpkg.exe install azure-storage-blobs-cpp:x64-windows 
+
+	if (!$?) {
+		throw "Vcpkg error $LASTEXITCODE, see log above."
+	}
+
+	# get CppRest SDK through vcgpk
+	&$VcpkgPath\vcpkg.exe install cpprestsdk:x64-windows
+
+	if (!$?) {
+		throw "Vcpkg error $LASTEXITCODE, see log above."
+	}
+
+} else {
+
+	$VcpkgPath = $Vcpkg
 }
 
-# bootstrap vcpkg
-&$VcpkgPath\bootstrap-vcpkg.bat
-
-# get Azure Storage SDK through vcgpk
-&$VcpkgPath\vcpkg.exe install azure-storage-blobs-cpp:x64-windows 
-
-if (!$?) {
-	throw "Vcpkg error $LASTEXITCODE, see log above."
-}
-
-# get CppRest SDK through vcgpk
-&$VcpkgPath\vcpkg.exe install cpprestsdk:x64-windows
-
-if (!$?) {
-	throw "Vcpkg error $LASTEXITCODE, see log above."
-}
+Write-Host ""
+Write-Host "Using vcpkg path '$VcpkgPath'"
+Write-Host ""
 
 Write-Host ""
 Write-Host "=== Running CMake ==="
@@ -81,7 +88,8 @@ $CMAKE_ARGS += "-G"
 
 if ($Solution -eq "vs2022") {
 	$CMAKE_ARGS += "Visual Studio 17 2022"
-} else {
+}
+else {
 	$CMAKE_ARGS += "Visual Studio 16 2019"
 }
 
